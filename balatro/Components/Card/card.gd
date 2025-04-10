e97bb1025b59e37a8bc97b0f509d0253de700450
Logan Croley <logan.croley.18@gmail.com>
2025-04-10 17:48:38 -0400

    added card hover rotation. added shadow movement

--- balatro/Components/Card/card.gd
+++ balatro/Components/Card/card.gd
@@ -1,21 +1,47 @@
 class_name Card
 extends Button
 
+@export var angle_x_max: float = 15.0
+@export var angle_y_max: float = 15.0
+@export var max_offset_shadow: float = 50.0
+
+
 var tween_hover: Tween
+var tween_rot: Tween
 var following_mouse: bool = false
+
+@onready var card_texture: TextureRect = $CardTexture
+@onready var shadow: TextureRect = $Shadow
 
 func _process(delta: float) -> void:
 	follow_mouse(delta)
+	handle_shadow(delta)
 
-func follow_mouse(_delta: float) ->void:
+func follow_mouse(_delta: float) -> void:
 	if not following_mouse: return
+	_reset_rotation()
 	var mouse_pos: Vector2 = get_global_mouse_position()
 	global_position = mouse_pos - (size/2)
 
+func handle_shadow(_delta) -> void:
+	var center: Vector2 = get_viewport_rect().size / 2
+	var dist: float = global_position.x - center.x
+	shadow.position.x = lerp(0.0, -sign(dist) * max_offset_shadow, abs(dist/(center.x)))
 
 func _on_gui_input(event: InputEvent) -> void:
 	handle_mouse_click(event)
-
+	if following_mouse: return
+	if not event is InputEventMouseMotion: return
+	var mouse_pos : Vector2 = get_local_mouse_position()
+	var lerp_val_x: float = remap(mouse_pos.x, 0.0, size.x, 0, 1)
+	var lerp_val_y: float = remap(mouse_pos.y,0.0, size.y,0,1)
+	var rot_x: float = lerp(-angle_x_max, angle_x_max, lerp_val_x)
+	var rot_y: float = lerp(angle_y_max, -angle_y_max, lerp_val_y)
+	
+	# setting the rotations amount on the card texture material
+	card_texture.material.set_shader_parameter("x_rot", rot_y)
+	card_texture.material.set_shader_parameter("y_rot", rot_x)
+	
 func handle_mouse_click(event) -> void:
 	if not event is InputEventMouseButton: return
 	if event.button_index != MOUSE_BUTTON_LEFT: return
@@ -24,14 +50,22 @@ func handle_mouse_click(event) -> void:
 	else:
 		following_mouse = true
 
-
 func _on_mouse_entered() -> void:
 	if tween_hover and tween_hover.is_running():
 		tween_hover.kill()
 	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
 	tween_hover.tween_property(self, "scale", Vector2(1.2,1.2), 0.5)
 
+func _reset_rotation() -> void:
+	if tween_rot and tween_rot.is_running():
+		tween_rot.kill()
+	tween_rot = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK).set_parallel(true)
+	tween_rot.tween_property(card_texture.material, "shader_parameter/x_rot",0.0, 0.5)
+	tween_rot.tween_property(card_texture.material, "shader_parameter/y_rot",0.0, 0.5)
+
 func _on_mouse_exited() -> void:
+	_reset_rotation()
+	
 	if tween_hover and tween_hover.is_running():
 		tween_hover.kill()
 	tween_hover = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
