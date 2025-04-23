4f3c794849f90ffae7f89da659a6c1dfbc9086db
Logan Croley <logan.croley.18@gmail.com>
2025-04-22 23:41:49 -0400

    stuff

--- obsidian-graph/components/attractor/attractor.gd
+++ obsidian-graph/components/attractor/attractor.gd
@@ -5,4 +5,10 @@ extends Area2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
+	Globals.gravity_disabled.connect(_disable_gravity)
 	collision_shape_2d.shape.radius = circ_radius
+	await get_tree().create_timer(5.0).timeout.connect(func(): Globals.gravity_disabled.emit())
+
+func _disable_gravity() -> void:
+	print("Attractor::_disable_gravity")
+	gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
--- obsidian-graph/components/obsidian_node/obsidian_node.gd
+++ obsidian-graph/components/obsidian_node/obsidian_node.gd
@@ -2,16 +2,42 @@ class_name ObsidianNode
 extends RigidBody2D
 
 @export var radius: int = 20
+@export var connected_nodes: Array[ObsidianNode] = []
 
 @onready var node_body = $NodeBody
 
-var _acceleration: Vector2
-
-var connected_nodes: Array[ObsidianNode] = []
+var _hovering: bool = false
+var _following_mouse: bool = false
 
 func _ready() -> void:
+	Globals.gravity_disabled.connect(_on_gravity_disabled)
 	node_body.shape.radius = radius
-	
-func _physics_process(delta: float) -> void:
-	print(linear_velocity)
-	move_and_collide(linear_velocity)
+
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, radius, Color.WHITE)
+	for node: ObsidianNode in connected_nodes:
+		draw_line(Vector2.ZERO, node.global_position,Color.WHITE)
+
+func _process(delta: float) -> void:
+	if _following_mouse:
+		global_position = get_global_mouse_position()
+	queue_redraw()
+
+func _on_gravity_disabled()-> void:
+	print("ObsidianNode::_on_gravity_disabled")
+	linear_damp = 10.0
+
+func _on_mouse_entered() -> void:
+	_hovering = true
+
+func _on_mouse_exited() -> void:
+	print("_on_mouse_exited")
+	_hovering = false
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("left_mouse") and _hovering:
+			_following_mouse = true
+		elif event.is_action_released("left_mouse") and _hovering:
+			print("asdfasdf")
+			_following_mouse = false
