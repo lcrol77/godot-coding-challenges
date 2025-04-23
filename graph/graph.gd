d0c0372367998ef94d29711d18ff4a4c06d97267
Logan Croley <logan.croley.18@gmail.com>
2025-04-23 14:30:03 -0400

    refactor

--- graph/graph.gd
+++ graph/graph.gd
@@ -3,9 +3,14 @@ extends Node3D
 @export var point_prefab: PackedScene
 @export var number_points: int = 10
 
+
+
 func _ready() -> void:
+	var position_vec = Vector3.ZERO;
+	var scale_vec = Vector3.ONE / 5.0;
 	for i in range(number_points):
 		var point: Node3D = point_prefab.instantiate()
 		add_child(point)
-		point.position = Vector3.RIGHT * (((i as float) + .5) / 5.0 - 1.0)
-		point.scale = Vector3.ONE / 5
+		position_vec.x = (((i as float) + .5) / 5.0 - 1.0)
+		point.position = position_vec
+		point.scale = scale_vec
