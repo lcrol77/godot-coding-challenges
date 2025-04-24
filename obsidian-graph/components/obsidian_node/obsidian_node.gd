cbf72a0a34d8f76669d1677674a9a364762490d4
Logan Croley <logan.croley.18@gmail.com>
2025-04-23 21:03:31 -0400

    node iteration

--- obsidian-graph/components/obsidian_node/obsidian_node.gd
+++ obsidian-graph/components/obsidian_node/obsidian_node.gd
@@ -4,38 +4,39 @@ extends RigidBody2D
 @export var radius: int = 20
 @export var connected_nodes: Array[ObsidianNode] = []
 
-@onready var node_body = $NodeBody
+@onready var node_body: CollisionShape2D = $NodeBody
+@onready var area_body: CollisionShape2D = $Area2D/AreaBody
 
 var _hovering: bool = false
 var _following_mouse: bool = false
+var _hover_tween: Tween
 
 func _ready() -> void:
 	Globals.gravity_disabled.connect(_on_gravity_disabled)
 	node_body.shape.radius = radius
-
-func _draw() -> void:
-	draw_circle(Vector2.ZERO, radius, Color.WHITE)
+	area_body.shape.radius = radius
 
 func _process(delta: float) -> void:
 	if _following_mouse:
 		global_position = get_global_mouse_position()
-	queue_redraw()
-
+		
 func _on_gravity_disabled()-> void:
 	print("ObsidianNode::_on_gravity_disabled")
 	linear_damp = 10.0
-
-func _on_mouse_entered() -> void:
-	_hovering = true
-
-func _on_mouse_exited() -> void:
-	print("_on_mouse_exited")
-	_hovering = false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("left_mouse") and _hovering:
 			_following_mouse = true
 		elif event.is_action_released("left_mouse") and _hovering:
-			print("asdfasdf")
 			_following_mouse = false
+
+func _on_mouse_entered() -> void:
+	_hovering = true
+	if _hover_tween and _hover_tween.is_running():
+		_hover_tween.kill()
+	
+
+func _on_mouse_exited() -> void:
+	print("_on_mouse_exited")
+	_hovering = false
--- obsidian-graph/components/obsidian_node_manager/obsidian_node_manager.gd
+++ obsidian-graph/components/obsidian_node_manager/obsidian_node_manager.gd
@@ -1,15 +1,17 @@
+@tool
 class_name ObsidianNodeMananger
 extends Node2D
 
 @export var node_prefab: PackedScene 
 
 func _ready():
-	pass
+	queue_redraw()
 
 func _draw() -> void:
 	for child: ObsidianNode in get_children():
 		if child.connected_nodes.size() == 0: continue
 		for connected_node: ObsidianNode in child.connected_nodes:
+			if connected_node == null: continue
 			draw_line(child.position, connected_node.position, Color(1,1,1,.5))
 
 func _process(delta: float) -> void:
