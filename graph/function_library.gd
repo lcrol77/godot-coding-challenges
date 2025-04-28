9979b772fcb34d86526cdbbf42b6c8ed49b98c30
Logan Croley <logan.croley.18@gmail.com>
2025-04-28 00:26:31 -0400

    added 3d

--- graph/function_library.gd
+++ graph/function_library.gd
@@ -4,15 +4,15 @@ enum FunctionName {Wave, MultiWave, Ripple}
 
 var functions: Array[Callable] = [wave, multi_wave, ripple]
 
-func wave(x: float, t: float) -> float:
+func wave(x: float, z: float, t: float) -> float:
 	return sin(PI * (x + t))
 
-func multi_wave(x: float, t: float) -> float:
+func multi_wave(x: float, z: float, t: float) -> float:
 	var y := sin(PI * (x + 0.5*t))
 	y += sin(2 * PI *(x+t)) * 0.5
 	return y * (2.0/3.0)
 
-func ripple(x: float, t: float) -> float:
+func ripple(x: float, z: float, t: float) -> float:
 	var d = abs(x)
 	var y = sin(PI*(4*d-t))
 	return y / (1 + 10 * d)
--- graph/graph.gd
+++ graph/graph.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node3D
 
 @export var point_prefab: PackedScene
@@ -13,19 +14,28 @@ func _ready() -> void:
 	var _position = Vector3.ZERO;
 	var _scale = Vector3.ONE * _step;
 	points = []
-	for i in range(resolution):
+	var x = 0
+	var z = 0
+	for i in range(resolution * resolution):
+		if x == resolution:
+			x = 0
+			z+=1
 		var point: CSGBox3D = point_prefab.instantiate()
 		points_container.add_child(point)
-		_position.x = (((i as float) + .5) * _step - 1.0)
+		_position.x = (((x as float) + .5) * _step - 1.0)
+		_position.z = (((z as float) + .5) * _step - 1.0)
 		point.position = _position
 		point.scale = _scale
 		point.material.set_shader_parameter("worldPos", _position)
 		points.append(point)
+		x += 1
 
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
 	var f: Callable = FunctionLibrary.get_function(function)
 	time += delta
 	for point: Node3D in points:
 		var pos: Vector3 = point.position
-		pos.y = f.call(pos.x, time)
+		pos.y = f.call(pos.x, pos.z, time)
 		point.position = pos
