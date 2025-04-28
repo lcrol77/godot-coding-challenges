a81ade59ca31f76e75c1aa24df7f68882224d3ee
Logan Croley <logan.croley.18@gmail.com>
2025-04-28 17:42:47 -0400

    implement z axis

--- graph/function_library.gd
+++ graph/function_library.gd
@@ -5,15 +5,17 @@ enum FunctionName {Wave, MultiWave, Ripple}
 var functions: Array[Callable] = [wave, multi_wave, ripple]
 
 func wave(x: float, z: float, t: float) -> float:
-	return sin(PI * (x + t))
+	return sin(PI * (x + z + t))
 
 func multi_wave(x: float, z: float, t: float) -> float:
 	var y := sin(PI * (x + 0.5*t))
-	y += sin(2 * PI *(x+t)) * 0.5
-	return y * (2.0/3.0)
+	y += sin(2 * PI *(z+t)) * 0.5
+	y += sin(PI * (x +z + 0.25 * t))
+
+	return y * (1.0/2.5)
 
 func ripple(x: float, z: float, t: float) -> float:
-	var d = abs(x)
+	var d = sqrt(x * x + z * z)
 	var y = sin(PI*(4*d-t))
 	return y / (1 + 10 * d)
 
