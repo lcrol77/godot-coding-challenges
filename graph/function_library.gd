d77529b6af826d8b8e135584b095cba4dd094741
Logan Croley <logan.croley@hii-tsd.com>
2025-04-30 15:04:58 -0400

    added torus and funny spheres

--- graph/function_library.gd
+++ graph/function_library.gd
@@ -1,8 +1,8 @@
 extends Node
 
-enum FunctionName {Wave, MultiWave, Ripple, ScalingSphere, VerticalBandsSphere}
+enum FunctionName {Wave, MultiWave, Ripple, ScalingSphere, VerticalBandsSphere, HorizontalBandsSphere, TwistingSphere, Torus}
 
-var functions: Array[Callable] = [wave, multi_wave, ripple, scaling_sphere,vertical_bands_sphere]
+var functions: Array[Callable] = [wave, multi_wave, ripple, scaling_sphere,vertical_bands_sphere,horiz_bands_sphere, twisting_sphere,torus]
 
 func get_function(func_name: FunctionName) -> Callable:
 	return functions[func_name]
@@ -48,5 +48,33 @@ func vertical_bands_sphere(u: float, v: float, t: float) -> Vector3:
 	var p = Vector3()
 	p.x = s * sin(PI * u)
 	p.y = r * sin(PI * .5 * v)
+	p.z = s * cos(PI*u)
+	return p
+
+func horiz_bands_sphere(u: float, v: float, t: float) -> Vector3:
+	var r = 0.9 + 0.1 *sin(PI* (4 * v + t))
+	var s = r * cos(.5 * PI *v)
+	var p = Vector3()
+	p.x = s * sin(PI * u)
+	p.y = r * sin(PI * .5 * v)
+	p.z = s * cos(PI*u)
+	return p
+
+func twisting_sphere(u: float, v: float, t: float) -> Vector3:
+	var r = 0.9 + 0.1 *sin(PI * (6 * u + 4 * v + t));
+	var s = r * cos(.5 * PI *v)
+	var p = Vector3()
+	p.x = s * sin(PI * u)
+	p.y = r * sin(PI * .5 * v)
+	p.z = s * cos(PI*u)
+	return p
+
+func torus(u: float, v: float, t: float) -> Vector3:
+	var r1 = 0.7 + 0.1 * sin(PI * (6 * u + 0.5 * t));
+	var r2 = 0.15 + 0.05 * sin(PI * (8 * u + 4 * v + 2 * t));
+	var s = r1 + r2 * cos(PI * v)
+	var p = Vector3()
+	p.x = s * sin(PI * u)
+	p.y = r2 * sin(PI  * v)
 	p.z = s * cos(PI*u)
 	return p
