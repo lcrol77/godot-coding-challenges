c00e3465529d0c7707b78179f7b878f7c8a136c9
Logan Croley <logan.croley.18@gmail.com>
2025-11-18 21:56:40 -0500

    added dnd component. added ItemData resource. Added Item. Added some scene stubs. added some resource impls

---
+++ inventory/scenes/item/item.gd
@@ -0,0 +1,38 @@
+@tool
+class_name Item
+extends Control
+
+@export var item_data: ItemData: set = set_item_data
+@onready var texture_rect: TextureRect = $TextureRect
+@onready var dnd: DragAndDrop = $Dnd
+
+var is_hovered: bool
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		dnd.drag_started.connect(_on_drag_started)
+		dnd.drag_canceled.connect(_on_drag_canceled)
+
+func _on_drag_started():
+	pass
+
+func _on_drag_canceled():
+	pass
+
+func set_item_data(val: ItemData) -> void:
+	item_data = val
+	if val == null:
+		return
+	if not is_node_ready():
+		await ready
+	texture_rect.texture = item_data.image
+
+func _on_mouse_entered() -> void:
+	if dnd.dragging:
+		return
+	is_hovered = true
+
+func _on_mouse_exited() -> void:
+	if dnd.dragging:
+		return
+	is_hovered = false
