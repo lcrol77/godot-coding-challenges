98f18238f8af7961fb30052cb143e53d9638d817
Logan Croley <logan.croley.18@gmail.com>
2025-11-18 22:42:49 -0500

    feat: added item_pedestal

---
+++ inventory/scenes/item_pedestal/item_pedestal.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+# preloaded item to instance off of 
+const ITEM_PRELOAD := preload("res://scenes/item/item.tscn")
+
+@export var item_data: ItemData
+
+func _ready() -> void:
+	assert(item_data, "Need to set item_data")
+	var display_item: Item = _instance_item()
+	add_child(display_item)
+
+func _instance_item() -> Item:
+	var display_item: Item = ITEM_PRELOAD.instantiate()
+	display_item.item_data = item_data.duplicate()
+	display_item.size = Vector2(24,24)
+	# Calculating offset of the border
+	display_item.position += Vector2(4,4)
+	return display_item
