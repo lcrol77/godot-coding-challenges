57981ed8f9d380c560307bc607fe5faeee20db2b
Logan Croley <logan.croley.18@gmail.com>
2025-11-22 18:02:43 -0500

    refactored item pedestal

--- inventory/scenes/item_pedestal/item_pedestal.gd
+++ inventory/scenes/item_pedestal/item_pedestal.gd
@@ -1,4 +1,5 @@
-extends TextureRect
+class_name ItemPedestal
+extends Control
 
 # preloaded item to instance off of 
 const ITEM_PRELOAD := preload("res://scenes/item/item.tscn")
@@ -13,7 +14,7 @@ func _ready() -> void:
 func _instance_item() -> Item:
 	var display_item: Item = ITEM_PRELOAD.instantiate()
 	display_item.item_data = item_data.duplicate()
-	display_item.size = Vector2(24,24)
+	display_item.size = Vector2(32,32)
 	# Calculating offset of the border
-	display_item.position += Vector2(4,4)
+	display_item.position += Vector2(5,5)
 	return display_item
