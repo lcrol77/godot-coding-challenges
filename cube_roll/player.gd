a7d8ac9ad4b576c4b535503a21fc43ee00921702
Logan Croley <logan.croley.18@gmail.com>
2026-01-06 20:30:03 -0500

    holy shit its working

--- cube_roll/player.gd
+++ cube_roll/player.gd
@@ -1,40 +1,56 @@
-class_name Player 
+class_name Player
 extends Node3D
 
-var rolling: bool = false
-var rotation_time: float = .25
-var size = 1.0
+var rolling := false
+@export var rotation_time := 0.25
+@export var size := 1.0  # cube edge length in world units
+@export var debug_marker := true
 
-@onready var marker: CSGSphere3D = $Marker
+@onready var marker: Node3D = $Marker
 
+var _start_t: Transform3D
+var _pivot: Vector3
+var _axis: Vector3
+var _angle := 0.0
 
 func _process(_delta: float) -> void:
 	if rolling:
 		return
-	# Movement input
-	if Input.is_action_pressed("ui_up"):
+
+	# Use just_pressed so you don't enqueue multiple rolls per key hold
+	if Input.is_action_just_pressed("ui_up"):
 		roll(Vector3.FORWARD)
-	if Input.is_action_pressed("ui_down"):
+	elif Input.is_action_just_pressed("ui_down"):
 		roll(Vector3.BACK)
-	if Input.is_action_pressed("ui_left"):
+	elif Input.is_action_just_pressed("ui_left"):
 		roll(Vector3.LEFT)
-	if Input.is_action_pressed("ui_right"):
+	elif Input.is_action_just_pressed("ui_right"):
 		roll(Vector3.RIGHT)
 
 func roll(dir: Vector3) -> void:
 	rolling = true
-	
-	var pivot:Vector3 = global_transform.origin + Vector3(dir.x * .5, -.5, dir.z * .5)
-	marker.global_position = pivot
-	
-	var axis:Vector3 = pivot.cross(dir).normalized()
-	var rot = transform.basis.rotated(axis, PI/2)
-	var start_q := Quaternion(transform.basis)
-	var end_q := Quaternion(rot)
+	dir = dir.normalized()
 
-	var tween = get_tree().create_tween()
-	tween.tween_method(apply_quat, start_q, end_q, rotation_time)
-	tween.finished.connect(func(): rolling  = false)
+	var half := size * 0.5
 
-func apply_quat(q: Quaternion) -> void:
-	transform.basis = Basis(q)
+	_start_t = global_transform
+	_pivot = _start_t.origin + (Vector3.DOWN * half) + (dir * half)
+	_axis  = Vector3.UP.cross(dir).normalized()
+	
+	_angle = PI * 0.5
+
+	if debug_marker:
+		marker.global_position = _pivot
+
+	var tween := get_tree().create_tween()
+	tween.tween_method(_apply_roll_t, 0.0, 1.0, rotation_time)
+	tween.finished.connect(func(): rolling = false)
+
+func _apply_roll_t(t: float) -> void:
+	var rot := Basis(_axis, _angle * t)
+
+	var origin := _start_t.origin
+	var rotated_origin := _pivot + (rot * (_start_t.origin - _pivot))
+	var rotated_basis := rot * _start_t.basis
+
+	global_transform = Transform3D(rotated_basis, rotated_origin)
